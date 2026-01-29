9ee2840da435f64eb2b15639b917b69875456fc8
makhlwf <altrhwnyashrf1@gmail.com>
2026-01-29 09:45:46 +0200

    part1 of renaming the app

--- source/application.py
+++ source/application.py
@@ -1,6 +1,6 @@
 # a module to hold application constants to be not re written everytime
 
-name = "Accessible Youtube Downloader Plus"
+name = "HexPlayer"
 version = "1.8.5"
 author = "Makhlwf"
 describtion = "يهدف هذا البرنامج إلى تقديم أيسر السبل أمام المستخدم الكفيف للتصفح والتنزيل من خدمة استضافة الفيديوهات youtube"
