ae3a7fd1668f54fecbd00b690e508d28f94f6478
makhlwf <altrhwnyashrf1@gmail.com>
2026-02-01 16:01:36 +0200

    bump version from 1.8.5 to 1.9.0

--- source/application.py
+++ source/application.py
@@ -1,6 +1,6 @@
 # a module to hold application constants to be not re written everytime
 
 name = "HexPlayer"
-version = "1.8.5"
+version = "1.9.0"
 author = "Makhlwf"
 describtion = "يهدف هذا البرنامج إلى تقديم أيسر السبل أمام المستخدم الكفيف للتصفح والتنزيل من خدمة استضافة الفيديوهات youtube"
