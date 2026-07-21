ae126bd9d8812f76ccc0022b568bd25cd3609e87
makhlwf <altrhwnyashrf1@gmail.com>
2026-07-21 07:35:17 +0200

    Bump app version to 3.5.0

--- packaging/windows/inno.iss
+++ packaging/windows/inno.iss
@@ -1,6 +1,6 @@
 #define MyAppName "HexPlayer"
 #define MyAppExeName "HexPlayer.exe"
-#define MyAppVersion "3.0.0"
+#define MyAppVersion "3.5.0"
 #define MyAppPublisher "makhlwf"
 #define MyAppURL "https://github.com/makhlwf/accessible_youtube_downloader_pro"
 #define RepoRoot "..\..\"
--- src/application.py
+++ src/application.py
@@ -1,6 +1,6 @@
 # a module to hold application constants to be not re written everytime
 
 name = "HexPlayer"
-version = "3.0.0"
+version = "3.5.0"
 author = "Makhlwf"
 description = "يهدف هذا البرنامج إلى تقديم أيسر السبل أمام المستخدم الكفيف للتصفح والتنزيل من خدمة استضافة الفيديوهات youtube"
